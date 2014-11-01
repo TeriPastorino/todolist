2defea75bb5d554e380aabce0d807d91c177b8e6
Teri Pastorino <tpasto@comcast.net>
2014-10-31 18:41:32 -0700

    routes and resources

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+  def index
+
+  end
+
+  def create
+     @task = Task.new(task_params)
+    if @task.save
+      render json: {:success =>  true, :task => @task}
+    else
+      render json: {:success => false}
+    end
+  end
+
+  def update
+  end
+
+  def index
+    @tasks = task.all
+    render :partial => 'index'
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
+
+private
+    def task_params
+      params.require(:task).permit(:title, :description, :archived, :priority)
+    end
